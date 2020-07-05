d5a156a36c9a50d0dc67b07b9fc89dd7fc5a135e
Kasperovich E.A <zhenkasp@mail.ru>
2020-07-05 13:12:21 +0300

    add user login and register view

--- users/views.py
+++ users/views.py
@@ -1,5 +1,28 @@
+from django.views import generic
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
 from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hello world!")
+    return render(request, 'users/index.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username = username, password = password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    context = { 'form': form }
+
+    return render(request, 'registration/register.html', context)
