72562c2aaa8f45607e66f9ee4befcc7045189ac1
Kasperovich E.A <zhenkasp@mail.ru>
2020-07-14 11:40:35 +0300

    add boards models List, Card

--- boards/models.py
+++ boards/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class List(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Card(models.Model):
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+    name = models.TextField(max_length=300, blank=True)
+
+    def __str__(self):
+        return self.name
