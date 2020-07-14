a39eb4d07f9c3a29700088dd0b508f196bb847e8
Kasperovich E.A <zhenkasp@mail.ru>
2020-07-14 12:41:14 +0300

    add admin List, Card; add output to boards/index

--- boards/admin.py
+++ boards/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import List, Card
+
+admin.site.register(List)
+admin.site.register(Card)
--- boards/views.py
+++ boards/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from .models import List, Card
 
 
 @login_required
 def index(request):
-    return render(request, 'boards/index.html')
+    lists = List.objects.all()
+    cards = Card.objects.all()
+    context = {'cards': cards, 'lists': lists}
+    return render(request, 'boards/index.html', context)
