35c3066ca3bf1911a3c7ee78a05d2d213e806583
Ole-Godspeed <vartanov88893@gmail.com>
2020-07-14 13:07:31 +0300

    Filter lists.object by user_id

--- boards/views.py
+++ boards/views.py
@@ -5,7 +5,7 @@ from .models import List, Card
 
 @login_required
 def index(request):
-    lists = List.objects.all()
+    lists = List.objects.filter(user_id=request.user.id)  #Filter lists by user_id
     cards = Card.objects.all()
     context = {'cards': cards, 'lists': lists}
     return render(request, 'boards/index.html', context)
