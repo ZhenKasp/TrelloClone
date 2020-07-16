f1741e245dc8f07a832f84afe4cc5d0a785a0881
Ole-Godspeed <vartanov88893@gmail.com>
2020-07-16 23:59:48 +0300

    adding Lists form boards is working

--- boards/views.py
+++ boards/views.py
@@ -1,30 +1,29 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import List, Card
 from django.http import HttpResponse
-
-from boards.models import List
+from boards.models import List, Card
 from boards.forms import ListForm
 
 
 @login_required
 def index(request):
+
     dictionary = {}
-    list1 = []
     lists = List.objects.filter(user_id=request.user.id)  #Filter lists by user_id
+
     for list in lists:
         cards = Card.objects.filter(list_id=list.id)
         dictionary[list.name] = cards
 
     if request.method == 'POST':
         form = ListForm(request.POST)
-        if form.is_valid(): # <-- doesn't go past here!
-            a = form.save(commit=False)
-            a.name = request.name
-            a.save()
-        list1 = List.objects.all()
+        form.user = 'admin'
+        print(form.is_valid())
+        if form.is_valid():
+            form.save()
+        context = {'dictionary': dictionary, 'form': form}
+    else:
+        context = {'dictionary': dictionary}
 
-    context = {'dictionary': dictionary, 'list': list1}
-    print(request.POST, context)
     return render(request, 'boards/index.html', context)
 # "lists" : { list1 : [card1, card2, car3] }
