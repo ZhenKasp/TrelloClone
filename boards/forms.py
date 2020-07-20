2cef4afff7c93af5e7b83b36386b5850f3e59035
Ole-Godspeed <vartanov88893@gmail.com>
2020-07-20 15:48:25 +0300

    adding CardForm to template

--- boards/forms.py
+++ boards/forms.py
@@ -1,9 +1,15 @@
 from django import forms
 from django.forms import ModelForm
-from boards.models import List
+from boards.models import List, Card
 
 
 class ListForm(ModelForm):
     class Meta:
         model = List
         fields = ['name'] #'__all__'
+
+
+class CardForm(ModelForm):
+    class Meta:
+        model = Card
+        fields = ['name', 'list'] #'__all__'
--- boards/views.py
+++ boards/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from boards.models import List, Card
-from boards.forms import ListForm
+from boards.forms import ListForm, CardForm
 
 
 @login_required
@@ -10,21 +10,30 @@ def index(request):
 
     dictionary = {}
     lists = List.objects.filter(user_id=request.user.id)  #Filter lists by user_id
-
     for list in lists:
         cards = Card.objects.filter(list_id=list.id)
         dictionary[list.name] = cards
 
     if request.method == 'POST':
-        form = ListForm(request.POST)
-        if form.is_valid():
-            a = form.save(commit=False)
+
+        form_card = CardForm(request.POST)
+        if form_card.is_valid():
+            a = form_card.save(commit=False)
             a.user = request.user
             a.save()
-            return redirect('index') #using GET request, for reloading the page without submitting form again
-    else:
-        form = ListForm()
+            return redirect('index')  # using GET request, for reloading the page without submitting form again
 
-    context = {'dictionary': dictionary, 'form': form}
+        form_list = ListForm(request.POST)
+        if form_list.is_valid():
+            a = form_list.save(commit=False)
+            a.user = request.user
+            a.save()
+            return redirect('index')
+
+    else:
+        form_list = ListForm()
+        form_card = CardForm()
+
+    forms_dict = {'form_list': form_list, 'form_card': form_card}
+    context = {'dictionary': dictionary, 'form': forms_dict} # dictionary == "lists" : { list1 : [card1, card2, car3] }
     return render(request, 'boards/index.html', context)
-# "lists" : { list1 : [card1, card2, car3] }
