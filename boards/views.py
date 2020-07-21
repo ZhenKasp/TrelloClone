105a78bb3edf598a2a2a80b3f507ea193e193dd9
Ole-Godspeed <vartanov88893@gmail.com>
2020-07-21 19:49:41 +0300

    working on AJAX 2

--- boards/views.py
+++ boards/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from boards.models import List, Card
-from boards.forms import ListForm, CardForm
+from boards.forms import ListForm, CardForm, EditList
+
+from django.http import JsonResponse
+from django.core import serializers
 
 
 @login_required
@@ -14,7 +17,25 @@ def index(request):
         cards = Card.objects.filter(list_id=list.id)
         dictionary[list] = cards
 
+    if request.is_ajax and request.method == "POST":
+        form_edit_list = EditList(request.POST)
+        if form_edit_list.is_valid():
+            instance = form_edit_list.save(commit=False)
+            instance.user = request.user
+            instance.save()
+            ser_instance = serializers.serialize('json', [instance, ])
+            return JsonResponse({"instance": ser_instance}, status=200)
+        else:
+            return JsonResponse({"error": form.errors}, status=400)
+
     if request.method == 'POST':
+
+        form_editl = EditList(request.POST)
+        if form_editl.is_valid():
+            a = List.objects.get(pk=1)
+            f = ListForm(request.POST, instance=a)
+            f.save()
+            return redirect('index')
 
         form_card = CardForm(request.POST)
         if form_card.is_valid():
@@ -33,7 +54,8 @@ def index(request):
     else:
         form_list = ListForm()
         form_card = CardForm()
+        form_editl = EditList()
 
-    forms_dict = {'form_list': form_list, 'form_card': form_card}
+    forms_dict = {'form_list': form_list, 'form_card': form_card, 'form_editl': form_editl}
     context = {'dictionary': dictionary, 'form': forms_dict} # dictionary == "lists" : { list1 : [card1, card2, car3] }
     return render(request, 'boards/index.html', context)
